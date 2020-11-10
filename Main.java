72441ae788587cc386f068ed94cd0e2d186d5db0
Tanya <tanya.leukhina@mail.ru>
2020-11-11 02:17:37 +0300

    java file added

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 12000;
+        int amount = 20;
+        int mile;
+        mile = ticketPrice / amount;
+        System.out.println(mile);
+    }
+}
+
