b4aa5a054f34ccaab01886f9391bf1d33b0bb1d5
Tanya <tanya.leukhina@mail.ru>
2020-11-11 02:25:42 +0300

    java edited

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int ticketPrice = 12000;
+        int ticketPrice = (int) 100.5;
         int amount = 20;
         int mile;
         mile = ticketPrice / amount;
